5ab6bc55ec049533bc7fe0781d9f1c813455545f
presia27 <pre.sia977@outlook.com>
2026-01-20 11:39:27 -0800

    Add documentation

--- src/collisionsys.ts
+++ src/collisionsys.ts
@@ -2,6 +2,10 @@ import { IEntity } from "./classinterfaces.ts";
 import { AbstractCollisionHandler } from "./componentLibrary/AbstractCollisionHandler";
 import { BoundingBox } from "./componentLibrary/boundingBox.ts";
 
+/**
+ * Describes a pair of entities which have collided
+ * and their bounding boxes
+ */
 interface CollisionPair {
   entityA: IEntity;
   entityB: IEntity;
@@ -9,6 +13,12 @@ interface CollisionPair {
   boundingBoxB: BoundingBox;
 }
 
+/**
+ * Collision system which monitors all
+ * collidable entities for collisions between
+ * them and signals their respective collision
+ * handlers of a collision event.
+ */
 export class CollisionSystem {
   private entities: IEntity[] = [];
 
@@ -16,6 +26,10 @@ export class CollisionSystem {
     this.entities.push(entity);
   }
 
+  /**
+   * Performs a narrow phase collision check
+   * on all entities registered with the system.
+   */
   public checkCollisions(): void {
     const collisionPairs: CollisionPair[] = [];
 
@@ -45,6 +59,11 @@ export class CollisionSystem {
     }
   }
 
+  /**
+   * Notifies entities that a collision occured with
+   * references to the other entity and bounding box
+   * @param pair CollisionPair with the 2 entities that collided
+   */
   private notifyCollisions(pair: CollisionPair): void {
     const handlerA = pair.entityA.getComponent(AbstractCollisionHandler);
     const handlerB = pair.entityB.getComponent(AbstractCollisionHandler);

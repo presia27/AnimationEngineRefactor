792e447f874bd1cd28f335070784853a048445e1
presia27 <pre.sia977@outlook.com>
2026-01-20 23:50:39 -0800

    Collision handling for sample cat on all four sides of a bounding box

--- js/samplefiles/catAsset/catCollisionHandler.js
+++ js/samplefiles/catAsset/catCollisionHandler.js
@@ -11,17 +11,29 @@ export class CatCollisionHandler extends AbstractCollisionHandler {
         const tag = oth.getComponent(BlueBoxTagFile);
         // handle collisions for the Blue Box
         if (tag && tag instanceof BlueBoxTagFile) {
-            if (this.boundingBox.getRight() > othBB.getLeft()) {
-                // this.movementCtl.setPosition({
-                //   x: this.movementCtl.getPosition().x - 1,
-                //   y: this.movementCtl.getPosition().y
-                // });
-                // this.movementCtl.setVelocityCommand({
-                //   direction: this.movementCtl.getCurrentDirection(),
-                //   magnitude: 0.01
-                // });
+            // calculate the middle of the other bounding box
+            const xMidPoint = (othBB.getLeft() + othBB.getRight()) / 2;
+            const yMidPoint = (othBB.getTop() + othBB.getBottom()) / 2;
+            if (this.boundingBox.getRight() > othBB.getLeft()
+                && this.boundingBox.getRight() < xMidPoint) {
+                // set position to the left of the object
                 this.movementCtl.setPosition({
                     x: othBB.getLeft() - this.sizeCtl.getWidth(),
+                    y: this.movementCtl.getPosition().y
+                });
+                // set the movement velocity to 0 on the x axis, allow movement on y
+                this.movementCtl.setVelocityCommand({
+                    direction: {
+                        x: 0,
+                        y: this.movementCtl.getCurrentDirection().y
+                    },
+                    magnitude: this.movementCtl.getSpeed()
+                });
+            }
+            else if (this.boundingBox.getLeft() < othBB.getRight()
+                && this.boundingBox.getLeft() > xMidPoint) {
+                this.movementCtl.setPosition({
+                    x: othBB.getRight(),
                     y: this.movementCtl.getPosition().y
                 });
                 this.movementCtl.setVelocityCommand({
@@ -32,24 +44,34 @@ export class CatCollisionHandler extends AbstractCollisionHandler {
                     magnitude: this.movementCtl.getSpeed()
                 });
             }
-            // else if (this.boundingBox.getLeft() < othBB.getRight()) {
-            //   this.movementCtl.setVelocityCommand({
-            //     direction: {x: 1, y: 0},
-            //     magnitude: 6
-            //   });
-            // }
-            // else if (this.boundingBox.getTop() < othBB.getBottom()) {
-            //   this.movementCtl.setVelocityCommand({
-            //     direction: {x: 0, y: 1},
-            //     magnitude: 6
-            //   });
-            // }
-            // else if (this.boundingBox.getBottom() > othBB.getTop()) {
-            //   this.movementCtl.setVelocityCommand({
-            //     direction: {x: 0, y: -1},
-            //     magnitude: 6
-            //   });
-            // }
+            else if (this.boundingBox.getTop() < othBB.getBottom()
+                && this.boundingBox.getTop() > yMidPoint) {
+                this.movementCtl.setPosition({
+                    x: this.movementCtl.getPosition().x,
+                    y: othBB.getBottom()
+                });
+                this.movementCtl.setVelocityCommand({
+                    direction: {
+                        x: this.movementCtl.getCurrentDirection().x,
+                        y: 0
+                    },
+                    magnitude: this.movementCtl.getSpeed()
+                });
+            }
+            else if (this.boundingBox.getBottom() > othBB.getTop()
+                && this.boundingBox.getBottom() < yMidPoint) {
+                this.movementCtl.setPosition({
+                    x: this.movementCtl.getPosition().x,
+                    y: othBB.getTop() - this.sizeCtl.getHeight()
+                });
+                this.movementCtl.setVelocityCommand({
+                    direction: {
+                        x: this.movementCtl.getCurrentDirection().x,
+                        y: 0
+                    },
+                    magnitude: this.movementCtl.getSpeed()
+                });
+            }
         }
     }
 }
--- src/samplefiles/catAsset/catCollisionHandler.ts
+++ src/samplefiles/catAsset/catCollisionHandler.ts
@@ -22,7 +22,12 @@ export class CatCollisionHandler extends AbstractCollisionHandler {
 
     // handle collisions for the Blue Box
     if (tag && tag instanceof BlueBoxTagFile) {
-      if (this.boundingBox.getRight() > othBB.getLeft()) {
+      // calculate the middle of the other bounding box
+      const xMidPoint = (othBB.getLeft() + othBB.getRight()) / 2;
+      const yMidPoint = (othBB.getTop() + othBB.getBottom()) / 2;
+      if (this.boundingBox.getRight() > othBB.getLeft()
+          && this.boundingBox.getRight() < xMidPoint
+        ) {
         // set position to the left of the object
         this.movementCtl.setPosition({
           x: othBB.getLeft() - this.sizeCtl.getWidth(),
@@ -38,24 +43,54 @@ export class CatCollisionHandler extends AbstractCollisionHandler {
           magnitude: this.movementCtl.getSpeed()
         });
       }
-      // else if (this.boundingBox.getLeft() < othBB.getRight()) {
-      //   this.movementCtl.setVelocityCommand({
-      //     direction: {x: 1, y: 0},
-      //     magnitude: 6
-      //   });
-      // }
-      // else if (this.boundingBox.getTop() < othBB.getBottom()) {
-      //   this.movementCtl.setVelocityCommand({
-      //     direction: {x: 0, y: 1},
-      //     magnitude: 6
-      //   });
-      // }
-      // else if (this.boundingBox.getBottom() > othBB.getTop()) {
-      //   this.movementCtl.setVelocityCommand({
-      //     direction: {x: 0, y: -1},
-      //     magnitude: 6
-      //   });
-      // }
+      else if (this.boundingBox.getLeft() < othBB.getRight()
+        && this.boundingBox.getLeft() > xMidPoint
+      ) {
+        this.movementCtl.setPosition({
+          x: othBB.getRight(),
+          y: this.movementCtl.getPosition().y
+        });
+
+        this.movementCtl.setVelocityCommand({
+          direction: {
+            x: 0,
+            y: this.movementCtl.getCurrentDirection().y
+          },
+          magnitude: this.movementCtl.getSpeed()
+        });
+      }
+      else if (this.boundingBox.getTop() < othBB.getBottom()
+        && this.boundingBox.getTop() > yMidPoint
+      ) {
+        this.movementCtl.setPosition({
+          x: this.movementCtl.getPosition().x,
+          y: othBB.getBottom()
+        });
+
+        this.movementCtl.setVelocityCommand({
+          direction: {
+            x: this.movementCtl.getCurrentDirection().x,
+            y: 0
+          },
+          magnitude: this.movementCtl.getSpeed()
+        });
+      }
+      else if (this.boundingBox.getBottom() > othBB.getTop()
+        && this.boundingBox.getBottom() < yMidPoint
+      ) {
+        this.movementCtl.setPosition({
+          x: this.movementCtl.getPosition().x,
+          y: othBB.getTop() - this.sizeCtl.getHeight()
+        });
+
+        this.movementCtl.setVelocityCommand({
+          direction: {
+            x: this.movementCtl.getCurrentDirection().x,
+            y: 0
+          },
+          magnitude: this.movementCtl.getSpeed()
+        });
+      }
     }
   }
 }
